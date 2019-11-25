7296c8d4caf4a6638dbb63f806f69804207e7cb8
mayukuse24 <vinaypoll@gmail.com>
2019-11-25 17:50:37 -0600

    Implemented getHistory of items for caller

--- lib/user.sol
+++ lib/user.sol
@@ -1,5 +1,7 @@
 pragma solidity ^0.5.11;
 
+pragma experimental ABIEncoderV2;
+
 import "./greencoin.sol";
 
 contract User is GreenCoin {
@@ -10,15 +12,29 @@ contract User is GreenCoin {
     }
 
     mapping (uint => Item) idToItem;
+    Item[] internal items;
 
     function createItem(uint qrHash) public {
         // TODO: Generate new hash for item based on qrHash (or use qrHash as itemId)
 
-        // Create item based on new hash and map to user
-        idToItem[qrHash] = Item(qrHash, msg.sender, false);
+        // Create item based on new hash
+        uint _id = items.push(Item(qrHash, msg.sender, false)) - 1;
+
+        // Map id to item
+        idToItem[qrHash] = items[_id];
     }
 
-    function getHistory() public view {
+    function getHistory() external view returns(Item[] memory) {
+        Item[] memory _itemHistory;
+        uint counter = 0;
 
+        for (uint i = 0; i < items.length; i++) {
+            if (items[i].creator == msg.sender) {
+                _itemHistory[counter] = items[i];
+                counter++;
+            }
+        }
+
+        return _itemHistory;
     }
 }
