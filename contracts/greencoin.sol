a6be8a0de886e0daee9aca67b97f665b12899eb5
mayukuse24 <vinaypoll@gmail.com>
2019-11-25 22:33:52 -0600

    Initialized truffle

---
+++ contracts/greencoin.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.8;
+
+contract GreenCoin {
+    mapping (address => uint) userToBalance;
+
+    function _incrementBalance(address userId, uint amount) private {
+        // TODO: Increment balance of userId by amount
+    }
+
+    function getBalance() public view {
+        // TODO: Return green coins for user
+    }
+}
---
+++ contracts/user.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.5.8;
+
+import "./greencoin.sol";
+
+contract User is GreenCoin {
+    struct Item {
+        uint id;
+        address creator;
+        bool isValidated;
+    }
+
+    mapping (uint => Item) idToItem;
+
+    function createItem(uint qrHash) public {
+        // TODO: Generate new hash for item based on qrHash (or use qrHash as itemId)
+
+        // TODO: Create item based on new hash
+
+        // TODO: Store item in itemToUser mapping
+    }
+
+    function getHistory() public view {
+
+    }
+}
