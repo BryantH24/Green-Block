2c57d0dcbf70f34f0194811e41da7c50d2923ac7
BryantH24 <gbhou5@gmail.com>
2020-01-20 15:10:00 -0600

    added post for login and database for transactions

--- mongo-server/server.py
+++ mongo-server/server.py
@@ -7,45 +7,76 @@ from web3 import Web3
 from web3.auto import w3
 
 from flask import Flask
+from flask import request
 
-cluster = MongoClient("mongodb+srv://GreenBlockServer:GreenestBlock123!@cluster0-tifxv.gcp.mongodb.net/test?retryWrites=true&w=majority")
+from time import gmtime, strftime
+
+cluster = MongoClient("mongodb+srv://GreenBlockServer:@cluster0-tifxv.gcp.mongodb.net/test?retryWrites=true&w=majority")
 db = cluster["GreenBlock"]
-collection = db["UserInfo"]
+logInDatabase = db["UserInfo"]
+transactionDatabase = db["transactionHistory"]
+
+ABIlist = ["sampleABI1", "sampleABI2"]
 
 
 oauthToken = "sampleOauth5"
 
-
-def createNewUser(oauthToken):
-    #check to see if account from oauthToken is already created
-    users = collection.find({"OAuth" : oauthToken})
-    for user in users:
-        if user != "":
-            return "account with your login credentials has already been created, please log in"
-
-    mnemonic = wallet.generate_mnemonic()
-    post = {"_id" : collection.count_documents({}) + 1 , "OAuth": oauthToken, "mnemonic": mnemonic}
-    w = wallet.create_wallet(network="ETH", seed=mnemonic, children=1)
-    collection.insert_one(post)
-    return True
-
+app = Flask(__name__)
 
 def logIn(oauthToken):
-    users = collection.find({"OAuth" : oauthToken})
+    users = logInDatabase.find({"OAuth" : oauthToken})
     for user in users:
         mnemonic = user["mnemonic"]
         print(mnemonic) #replace with blockchain stuff (brownie?)
-        if mnemonic != "":
-            return True
+        return mnemonic
+# @app.route('/login/<oauthToken>')
+
+@app.route('/login', methods=['POST'])
+def createNewUser():
+    #check to see if account from oauthToken is already created
+    oauthJson = request.get_json()
+    oauthToken = oauthJson["oauthToken"]
+
+    users = logInDatabase.find({"OAuth" : oauthToken})
+    for user in users:
+        if user != "":
+            mnenomic = logIn(oauthToken)
+            return mnenomic
         else:
-            return False
-logIn(oauthToken)
+            mnemonic = wallet.generate_mnemonic()
+            post = {"_id" : logInDatabase.count_documents({}) + 1 , "OAuth": oauthToken, "mnemonic": mnemonic, "userStatus": "User"}
+            w = wallet.create_wallet(network="ETH", seed=mnemonic, children=1)
+            logInDatabase.insert_one(post)
+            return True
 
 
 #use web3py
-def createObject(mnemonic, qrHash):
+@app.route('/createItem', methods=['POST'])
+def createItem():
+    oauthJson = request.get_json()
+    oauthToken = oauthJson["oauthToken"]
+    qrHash = oauthJson["qrHash"]
+    mnemonic = logIn(oauthToken)
     w = wallet.create_wallet(network="ETH", seed=mnemonic, children=1)
-    GreenBlock = w3.eth.contract(address=w['address'], abi=ABI)
-    myContract.caller.createItem(qrHash)
+    # GreenBlockCreateItem = w3.eth.contract(address=w['address'], abi=ABIlist[0])
+    # return GreenBlockCreateItem.caller.createItem(qrHash)
 
-createObject("small recipe science nation blind multiply filter cost outer large dash vault", 1231232)
+    post = {"_id" : transactionDatabase.count_documents({}) + 1 , "OAuth": oauthToken, "mnemonic": mnemonic, "qrHash": qrHash, "time" : strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())}
+    transactionDatabase.insert_one(post)
+    return "logged on database"
+
+@app.route('/getHistory', methods=['POST'])
+def getHistory(oauthToken):
+    oauthJson = request.get_json()
+    oauthToken = oauthJson["oauthtoken"]
+    mnemonic = logIn(oauthToken)
+    w = wallet.create_wallet(network="ETH", seed=mnemonic, children=1)
+    GreenBlockCreateItem = w3.eth.contract(address=w['address'], abi=ABIlist[1])
+    return GreenBlockCreateItem.caller.getHistory()
+
+# run the app.
+if __name__ == "__main__":
+    # Setting debug to True enables debug output. This line should be
+    # removed before deploying a production app.
+    app.debug = True
+    app.run()
