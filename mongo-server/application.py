600f40af7ae4849078b14f69c974730aab523af1
mayukuse24 <vinaypoll@gmail.com>
2020-01-26 19:45:58 -0600

    Updated getBalance and getHistory

--- mongo-server/application.py
+++ mongo-server/application.py
@@ -1,17 +1,12 @@
+from time import gmtime, strftime
+import json
+
+from flask import Flask, jsonify
+from flask import request
 import pymongo
 from pymongo import MongoClient
-
 from pywallet import wallet
-
-from web3 import Web3
-from web3.auto import w3
-
-from flask import Flask
-from flask import request
-
-from time import gmtime, strftime
-
-import json
+from web3 import Web3, HTTPProvider
 
 
 ##w["xprivate_key"] is the private key for the created wallet
@@ -20,12 +15,15 @@ db = cluster["GreenBlock"]
 logInDatabase = db["UserInfo"]
 transactionDatabase = db["transactionHistory"]
 
-ABI = json.load(open("Validator.json", "r"))
+ABI = json.load(open("Validator.json", "r"))['abi']
+
 contractAddress = Web3.toChecksumAddress("0x41F5a83AA42B86a6a3da525A96D91a95E7e82c94")
 
 oauthToken = "sampleOauth5"
-w3 = Web3(HTTPProvider("https://ropsten.infura.io/v3/8f534b2f394a48a68f99f56dd61db9b3%22"))
+w3 = Web3(HTTPProvider("https://ropsten.infura.io/v3/8f534b2f394a48a68f99f56dd61db9b3"))
 app = Flask(__name__)
+
+GreenBlockInstance = w3.eth.contract(address=contractAddress, abi=ABI)
 
 def logIn(oauthToken):
     users = logInDatabase.find({"OAuth" : oauthToken})
@@ -67,7 +65,7 @@ def createItem():
     qrHash = oauthJson["qrHash"]
     mnemonic = logIn(oauthToken)
     w = wallet.create_wallet(network="ETH", seed=mnemonic, children=1)
-    GreenBlockCreateItem = w3.eth.contract(address=contractAddress, abi=ABI["abi"])
+    GreenBlockCreateItem = w3.eth.contract(address=contractAddress, abi=ABI)
     post = {"_id" : transactionDatabase.count_documents({}) + 1 , "OAuth": oauthToken, "mnemonic": mnemonic, "qrHash": qrHash, "time" : strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())}
     transactionDatabase.insert_one(post)
 
@@ -78,33 +76,39 @@ def createItem():
 
 @app.route('/getHistory', methods=['POST'])
 def getHistory():
+    global GreenBlockInstance
+
     oauthJson = request.get_json()
     oauthToken = oauthJson["oauthtoken"]
     mnemonic = logIn(oauthToken)
+
+    print(mnemonic)
+
     w = wallet.create_wallet(network="ETH", seed=mnemonic, children=1)
-    GreenBlockGetHistory = w3.eth.contract(address=contractAddress, abi=ABI["abi"])
+    
+    print(w)
 
-    getHistoryTransaction = GreenBlockGetHistory.functions.getHistory(w["address"]).buildTransaction({'chainId': 1,'gas': 100000,'gasPrice': GreenBlockGetHistory.functions.getHistory().estimateGas(),'nonce': 0})
+    userAddress = Web3.toChecksumAddress(w['address'])
 
-    signedGetHistory = w3.eth.account.sign_transaction(getHistoryTransaction, private_key=w["xprivate_key"])
-    return w3.eth.sendRawTransaction(signedGetHistory.rawTransaction)
+    itemIds, itemStates = GreenBlockInstance.caller.getHistory('0x51499e950B01aF5B74Ba602b4f47361940B4dc0d')
+
+    return jsonify({'items': itemIds, 'states': itemStates}), 200
 
 @app.route('/getBalance', methods=['POST'])
 def getBalance():
+    global GreenBlockInstance
+
     oauthJson = request.get_json()
     oauthToken = oauthJson["oauthtoken"]
     mnemonic = logIn(oauthToken)
+
     w = wallet.create_wallet(network="ETH", seed=mnemonic, children=1)
-    GreenBlockGetBalance = w3.eth.contract(address=contractAddress, abi=ABI["abi"])
-    transactions = transactionDatabase.find("_id")
-    itemList = []
-    for item in transactions:
-        itemList.append(item)
 
-    getBalanceTransaction = GreenBlockGetBalance.functions.getBalance().buildTransaction({'chainId': 1,'gas': 100000,'gasPrice': GreenBlockGetBalance.functions.getBalance().estimateGas(),'nonce': 0})
+    userAddress = Web3.toChecksumAddress(w['address'])
 
-    signedGetBalance = w3.eth.account.sign_transaction(getBalanceTransaction, private_key=w["xprivate_key"])
-    return w3.eth.sendRawTransaction(signedGetBalance.rawTransaction)
+    balance = GreenBlockInstance.caller.getBalance(userAddress)
+
+    return jsonify({'balance': balance}), 200
 
 @app.route('/validateItem', methods=['POST'])
 def validateItem():
@@ -112,21 +116,20 @@ def validateItem():
     oauthToken = oauthJson["oauthtoken"]
     mnemonic = logIn(oauthToken)
     w = wallet.create_wallet(network="ETH", seed=mnemonic, children=1)
-    GreenBlockValidateItem = w3.eth.contract(address=contractAddress, abi=ABI["abi"])
 
     validateItemTransaction = GreenBlockValidateItem.functions.validateItem(qrHash).buildTransaction({'chainId': 1,'gas': 100000,'gasPrice': GreenBlockValidateItem.functions.validateItem(qrHash).estimateGas(),'nonce': 0})
 
     signedValidateItem = w3.eth.account.sign_transaction(validateItemTransaction, private_key=w["xprivate_key"])
     return w3.eth.sendRawTransaction(signedValidateItem.rawTransaction)
 
-
-
 @app.route("/")
 def home():
     return "Hello, World!"
+
 # run the app.
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
     app.debug = True
     app.run(host = '0.0.0.0', port = 5000)
+
