a57c0ccc5df424420d20b568cc5b58ee0f5bf97c
BryantH24 <gbhou5@gmail.com>
2020-04-25 19:55:34 -0400

    added the rest of the helper functions

--- mongo-server/mongoDBhelper.py
+++ mongo-server/mongoDBhelper.py
@@ -2,12 +2,28 @@ import pymongo
 from pymongo import MongoClient
 from time import gmtime, strftime
 import json
+from flask import Flask, jsonify
 
 mongoUrl = "mongodb+srv://user:password12345@cluster0-3s8ia.gcp.mongodb.net/test?retryWrites=true&w=majority"
 cluster = MongoClient(mongoUrl)
 db = cluster["GreenBlock"]
 userDatabase = db["UserInfo"]
 transactionDatabase = db["transactionHistory"]
+
+def createNewUser(oauthToken):
+    post = {
+            "_id" : userDatabase.count_documents({}) + 1 ,
+            "OAuth": oauthToken,
+            "balance" : 0,
+            "status" : "User"
+            }
+    userDatabase.insert_one(post)
+
+def login(oauthToken):
+    try:
+        user = userDatabase.find({"OAuth" : oauthToken})[0]
+    except IndexError:
+        createNewUser(oauthToken)
 
 def changeUserBalance(oauthToken, changeVal):
     user = userDatabase.find({"OAuth" : oauthToken})[0]
@@ -26,6 +42,7 @@ def getUserBalance(oauthToken):
     if user["balance"] == "":
         return 0
     else:
+        print(user["balance"])
         return user["balance"]
 
 def getHistory(oauthToken):
@@ -35,13 +52,42 @@ def getHistory(oauthToken):
         transArr.append(trans)
     print(transArr)
 
-def addTransaction(oauthToken, qrhash):
-    post = {
+def createItem(oauthToken, qrhash):
+    try:
+        trans = transactionDatabase.find({"qrhash" : qrhash})[0]
+        return False
+    except IndexError:
+        post = {
                 "_id" : transactionDatabase.count_documents({}) + 1 ,
                 "OAuth": oauthToken,
-                # "mnemonic": mnemonic,
-                "qrHash": qrHash,
-                "time" : strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
-            }
-    transactionDatabase.insert_one(post)
-    return jsonify({ 'transactionId': str(transactionDatabase.count_documents({}) + 1), 'timeCreated' : strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())}), 201
+                "qrhash": qrhash,
+                "time" : strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime()),
+                "status" : "unvalidated"
+                }
+        transactionDatabase.insert_one(post)
+        return True
+    # return jsonify({ 'transactionId': str(transactionDatabase.count_documents({}) + 1), 'timeCreated' : strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())}), 201
+
+
+def validateItem(qrhash, value):
+    try:
+        trans = transactionDatabase.find({"qrhash" : qrhash})[0]
+        if trans['status'] == "validated":
+            # bottle has already bene validated
+            return False
+        else:
+            transactionDatabase.update_one({
+              '_id': trans["_id"]
+            },{
+              '$set': {
+                'status': "validated"
+              }
+            }, upsert=False)
+            changeUserBalance(trans["OAuth"], value)
+    except IndexError:
+        return False
+
+login("test1")
+createItem("test1", 12)
+validateItem(12, 5)
+getUserBalance("test1")
